6a4421ec60343b76623e1b16899376caeeb8e076
AidenBow <AidenBow@users.noreply.github.com>
2019-05-30 19:45:51 -0700

    added student and project manager class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -24,8 +24,46 @@ class Instructor extends Person {
     console.log(`Today we are learning about ${subject}`)
   }
 
-  grade(subject){
+  grade(student, subject){
     console.log(`${student.name} receives a perfect score on ${subject}`)
   }
 }
 
+class Student extends Person {
+  constructor(attrs){
+    super(attrs);
+    this.previousBackground = attrs.previousBackground;
+    this.className = attrs.className;
+    this.favSubjects = attrs.favSubjects;
+  }
+
+  listsSubjects(){
+    this.forEach(function(subject) {
+      console.log(subject)
+    })
+  }
+
+  PRAssignment(subject){
+    console.log(`${student.name} has submitted a PR for ${subject}`)
+  } 
+
+  sprintChallenge(subject){
+    console.log(`${student.name} has begun sprint challenge on ${subject}`)
+  }
+}
+
+class ProjectManager extends Instructor {
+  constructor(attrs){
+    super(attrs);
+    this.gradClassName = attrs.gradClassName;
+    this.favInstructor = attrs.favInstructor;
+  }
+
+  standUp(channel){
+    console.log(`${this.name} announces to ${channel}, @channel standy times!​​​​​`)
+  }
+
+  debugsCode(student, subject){
+    console.log(`${this.name} debugs ${student.name}'s code on ${subject}`)
+  }
+}
