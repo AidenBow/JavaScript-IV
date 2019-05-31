71846ec8db5f56b08046d0894d72e0bf9bdac917
AidenBow <AidenBow@users.noreply.github.com>
2019-05-30 19:13:33 -0700

    added person and instructor class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,31 @@
 // CODE here for your Lambda Classes
+
+class Person {
+  constructor(attrs){
+    this.name = attrs.name;
+    this.age = attrs.age;
+    this.location = attrs.location;
+  }
+
+  speak(){
+    console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+  }
+}
+
+class Instructor extends Person {
+  constructor(attrs){
+    super(attrs);
+    this.specialty = attrs.specialty;
+    this.favLanguage = attrs.favLanguage;
+    this.catchPhrase = attrs.catchPhrase;
+  }
+
+  demo(subject){
+    console.log(`Today we are learning about ${subject}`)
+  }
+
+  grade(subject){
+    console.log(`${student.name} receives a perfect score on ${subject}`)
+  }
+}
+
